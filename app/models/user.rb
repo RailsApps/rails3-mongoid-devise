d410fa756fb60a3c3e82bf6f99e6a155a0e716b2
Daniel Kehoe <kehoe@fortuity.com>
2010-06-18 15:29:19 -0700

    seed the database with a default user (and remove the unimplemented Mongoid attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,8 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  field :name, :accessible => false
-  field :email, :accessible => false
-  field :password, :accessible => false
-  field :password_confirmation, :accessible => false
-  validates_presence_of :name, :email
+  field :name
+  validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
 
 end
