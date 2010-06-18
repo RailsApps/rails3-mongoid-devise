e97dae30834995ec1b0a4b1bee374e4a44eaf969
Daniel Kehoe <kehoe@fortuity.com>
2010-06-18 15:01:15 -0700

    create a home controller and view

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Rails3MongoidDevise
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
