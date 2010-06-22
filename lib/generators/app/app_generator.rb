f7d91a35b43fc1b262189e8b675eb3c8acf42d28
Daniel Kehoe <kehoe@fortuity.com>
2010-06-21 21:01:00 -0700

    Revert "Initial commit"
    This reverts commit a7aaac14122b7efdc7bb73a1163388015c46ef6e.

--- lib/generators/app/app_generator.rb
+++
@@ -1,9 +0,0 @@
-class AppGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  puts "Testing..."
-  def setup_git
-    git :init
-    git :add => "."
-    git :commit => "-a -m 'Initial commit'"
-  end
-end
