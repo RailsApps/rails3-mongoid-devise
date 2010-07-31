599ef4104ba0ccd0e743d0812d727ed5fcbd8df4
Daniel Kehoe <kehoe@fortuity.com>
2010-07-31 14:34:03 -0700

    update to use mongoid 2.0.0.beta.15 and devise 1.1.1

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0.rc'
-gem 'mongoid', '2.0.0.beta11'
+gem 'mongoid', '2.0.0.beta.15'
 gem 'bson_ext', '1.0.4'
-gem 'devise', '1.1.0'
+gem 'devise', '1.1.1'
 # uncomment if you wish to deploy to Heroku
 # gem 'heroku', '1.9.12'
--- template.rb
+++ template.rb
@@ -49,7 +49,7 @@ end
 puts "setting up Gemfile for Mongoid..."
 gsub_file 'Gemfile', /gem \'sqlite3-ruby/, '# gem \'sqlite3-ruby'
 append_file 'Gemfile', "\n# Bundle gems needed for Mongoid\n"
-gem 'mongoid', '2.0.0.beta11'
+gem 'mongoid', '2.0.0.beta.15'
 gem 'bson_ext', '1.0.4'
 
 puts "installing Mongoid gems (takes a few minutes!)..."
