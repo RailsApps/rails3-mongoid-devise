b53348dfcd64800d437cb2649704e6c58161a5de
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 14:32:58 -0800

    update config/application.rb for newer Mongoid

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-# If you are deploying to Heroku and MongoHQ,
-# you supply connection information here.
-require 'uri'
-if ENV['MONGOHQ_URL']
-  mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
-  ENV['MONGOID_HOST'] = mongo_uri.host
-  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
-  ENV['MONGOID_USERNAME'] = mongo_uri.user
-  ENV['MONGOID_PASSWORD'] = mongo_uri.password
-  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
-end
-
-require 'mongoid/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'active_resource/railtie'
-require 'rails/test_unit/railtie'
-
+# Pick the frameworks you want:
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -50,15 +38,8 @@ config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
-      config.generators do |g|
-        g.orm             :mongoid
-      end
-
-      # Configure the default encoding used in templates for Ruby 1.9.
-
+    # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
