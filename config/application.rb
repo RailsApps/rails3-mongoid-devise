6c3410ac9c17a8f5b256bd5f5ab4f7b7cc94c90e
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 14:19:42 -0800

    change for Prototype to jQuery Javascript framework

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Rails3MongoidDevise
   class Application < Rails::Application
+  
+config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
