8d0778cd2eddc34ce4df5fc3c6fa1911a999ca2a
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 15:53:17 -0800

    update comments to match Devise version 1.2rc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
