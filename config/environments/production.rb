0ad981ab84c6345e07ec42f8516f63e3636fc81e
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 14:31:37 -0800

    newer config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,4 @@ Rails3MongoidDevise::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 end
