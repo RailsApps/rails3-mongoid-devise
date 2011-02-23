307202c5d7649e27918c1f0ce1a66a2c37f0be4b
Daniel Kehoe <kehoe@fortuity.com>
2011-02-23 14:33:51 -0800

    newer config files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ Rails3MongoidDevise::Application.config.session_store :cookie_store, :key => '_r
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # Rails3MongoidDevise::Application.config.session_store :active_record_store
