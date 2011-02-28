b8b8d4f0db43a6af2c68f78f223410658fb0526b
Daniel Kehoe <kehoe@fortuity.com>
2011-02-28 13:08:10 -0800

    resolve issue 17, change YAML Engine to avoid fail validating database name, thanks Carsten Block

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 
 # Initialize the rails application
 Rails3MongoidDevise::Application.initialize!
