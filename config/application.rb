9a45f5eec85341589760a16a1d67bff897acc9a3
Daniel Kehoe <kehoe@fortuity.com>
2011-03-01 13:25:05 -0800

    minor rearranging of application.rb file

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Rails3MongoidDevise
   class Application < Rails::Application
-  
-config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -38,6 +36,8 @@ config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
