014391902afeb7946355f2d762bebdc4eb565a04
Daniel Kehoe <kehoe@fortuity.com>
2011-03-01 13:26:20 -0800

    no need for the test_unit/railtie in the application.rb with RSpec

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,7 +13,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Rails3MongoidDevise
   class Application < Rails::Application
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
