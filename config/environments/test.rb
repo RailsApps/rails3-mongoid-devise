98a1b86d61c81d9ed4b2286be723390da03079cf
Daniel Kehoe <daniel@danielkehoe.com>
2013-02-20 16:50:48 -0800

    build using rails_apps_composer 2.2.34

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,7 @@ Rails3MongoidDevise::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
 
